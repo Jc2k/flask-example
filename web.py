d1080ad77e5f6bb7153302e2a5567c7a74b251dd
John Carr <john.carr@isotoma.com>
2011-09-29 23:52:29 +0100

    Example of problem

---
+++ web.py
@@ -0,0 +1,13 @@
+import os
+from brokenegg import get_important_message
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return get_important_message()
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
